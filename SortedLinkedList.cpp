70de10f58a390591f5d96f8b0e0091925a0333f6
razazke <dylanarcher2740@gmail.com>
2020-04-02 14:30:22 -0500

    Changed template class keyword to typename keyword, and added a template class command to be able to template ints

--- SortedLinkedList.cpp
+++ SortedLinkedList.cpp
@@ -6,52 +6,52 @@ Purpose:
 
 #include "SortedLinkedList.h"
 
-template<class ItemType>
+template<typename ItemType>
 int SortedLinkedList<ItemType>::getPosition(const ItemType &anEntry) const {
     return 0;
 }
 
-template<class ItemType>
+template<typename ItemType>
 SortedLinkedList<ItemType>::SortedLinkedList() {
-size = 0;
 }
 
-template<class ItemType>
+template<typename ItemType>
 SortedLinkedList<ItemType>::~SortedLinkedList() {
 
 }
 
-template<class ItemType>
+template<typename ItemType>
 bool SortedLinkedList<ItemType>::insertSorted(const ItemType & anEntry) {
     return false;
 }
 
-template<class ItemType>
+template<typename ItemType>
 bool SortedLinkedList<ItemType>::removeSorted(const ItemType & anEntry) {
     return false;
 }
 
-template<class ItemType>
+template<typename ItemType>
 bool SortedLinkedList<ItemType>::isEmpty() const {
     return false;
 }
 
-template<class ItemType>
+template<typename ItemType>
 int SortedLinkedList<ItemType>::getLength() const {
     return 0;
 }
 
-template<class ItemType>
+template<typename ItemType>
 bool SortedLinkedList<ItemType>::remove(int position) {
     return false;
 }
 
-template<class ItemType>
+template<typename ItemType>
 ItemType SortedLinkedList<ItemType>::getEntry(int position) const {
-    return nullptr;
+    return position;
 }
 
-template<class ItemType>
+template<typename ItemType>
 void SortedLinkedList<ItemType>::clear() {
 
 }
+template class SortedLinkedList<int>;
